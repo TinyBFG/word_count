ef43937a1920630222d93eb8165f8f4a3e7e8a55
TinyBFG <tinybfg337@gmail.com>
2022-03-21 21:14:00 -0500

    WordCount and test are working, still need to figure out how to test IO.gets

--- lib/word_count.ex
+++ lib/word_count.ex
@@ -1,13 +1,21 @@
 defmodule WordCount do
 
   def intro() do
-    filename = IO.gets("Hello!  Welcome to wordcount, what file would you like to count\n")
-            |> String.trim()
-            |> IO.inspect()
-            #|> counter()
+    IO.gets("Hello!  Welcome to wordcount, what file would you like to count?\n")
+    |> String.trim()
+    |> format_path()
+    |> counter()
+  end
+
+  def format_path(filename) do
+    Path.expand("../#{filename}")
   end
 
   def counter(filename) do
-
+    File.read!(filename)
+    |> String.split(~r{(\\n|[^\w'])+})
+    |> Enum.map(fn x -> String.trim(x) end)
+    |> Enum.filter(fn x -> x != "" end)
+    |> Enum.count()
   end
 end
--- test/word_count_test.exs
+++ test/word_count_test.exs
@@ -2,7 +2,11 @@ defmodule WordCountTest do
   use ExUnit.Case
   doctest WordCount
 
-  test "greets the world" do
-    assert WordCount.hello() == :world
+  test "counter/1 returns the correct number of words for the file" do
+    filename = Path.expand("../word_count/mix.exs")
+
+    count = WordCount.counter(filename)
+
+    assert count == 71
   end
 end
